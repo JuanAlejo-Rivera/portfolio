f25aa2f23a572401aa76a07907b45902b19511d4
Alejandro Rivera <juan.alejorivera25@gmail.com>
2025-12-22 10:26:37 -0500

    minor correction of information in gamercore

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -58,7 +58,7 @@ const projects: Project[] = [
         title: "Eccommerce, GamerCore",
         description: `Plataforma de comercio electrónico para productos gamer, con catálogo dinámico y carrito de compras persistente.`,
         fulldescription: "Aplicación de comercio electrónico enfocada en productos gamer y componentes de PC. Permite a los usuarios explorar un catálogo dinámico, gestionar un carrito de compras persistente y calcular automáticamente el total de sus pedidos.",
-        tecnologias: "🚀 Tecnologías: React, TypeScript, Zustand, TailwindCSS, Node.js, MongoDB.",
+        tecnologias: "🚀 React, TypeScript, Vite, TailwindCSS, Framer Motion, Lucide React, React Hot Toast, React Router DOM, ESLint.",
         images: ["img/gamercore/newCatalog.jpg", "img/gamercore/newMAin.jpg", "img/gamercore/newBurgMenu.jpg", "img/gamercore/carBurgMenu.jpg", "img/gamercore/notifications.jpg"],
         deploy: "https://gamer-core-ecommerce.vercel.app/",
         repository: "https://github.com/JuanAlejo-Rivera/Gamer-Core-ecommerce",
