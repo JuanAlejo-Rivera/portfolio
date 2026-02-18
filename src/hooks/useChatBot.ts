3963597fe944d51fbeb7b27c04cfaff65b139b8f
Alejandro Rivera <juan.alejorivera25@gmail.com>
2026-02-18 09:15:21 -0500

    chatbot update

--- src/hooks/useChatBot.ts
+++ src/hooks/useChatBot.ts
@@ -7,7 +7,7 @@ export const useChatBot = () => {
 
     useEffect(() => {
         createChat({
-            webhookUrl: 'https://primary-production-ec56.up.railway.app/webhook/35ebee41-0e73-456e-ad86-e172bb6f5080/chat',
+            webhookUrl: 'https://primary-production-59094.up.railway.app/webhook/35ebee41-0e73-456e-ad86-e172bb6f5080/chat',
             webhookConfig: {
                 method: 'POST',
                 headers: {},
